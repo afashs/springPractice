2e96a5397e4a525f283d6b7f5d7b6a4fd16c866f
marvin.seo <marvin.seo@kakaoenterprise.com>
2022-07-03 20:36:03 +0900

    [test][applicationContext] 동일타입 중복에 대한 테스트 코드

---
+++ src/test/java/com/example/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,45 @@
+package com.example.core.beanfind;
+
+import com.example.core.AppConfig;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 빈 타입으로 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("[o] 구체타입으로 조회")
+    void findBeanByTypeDetailOK() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("[x] 빈 이름으로 조회")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class)
+        );
+    }
+}
